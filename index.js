afa291787f9406d220197d07383cd2b2c3b67089
Gosha <gosha_355@mail.ru>
2023-08-03 13:54:40 +0200

    Ready task

---
+++ index.js
@@ -0,0 +1,62 @@
+const inputEl = document.querySelector('input');
+const buttonEl = document.querySelector('button');
+const timerEl = document.querySelector('span');
+
+let createTimerAnimatorHandler = null
+console.log(createTimerAnimatorHandler);
+// Напишите реализацию createTimerAnimator
+// который будет анимировать timerEl
+const createTimerAnimator = () => {  
+  return (seconds) => {createTimerAnimatorHandler = setInterval(function() {
+      let formattedSeconds = seconds % 60
+      let minutes = seconds /60 % 60
+      let hours = seconds / 60 / 60 % 60
+      let formattedMinutes = Math.trunc(minutes)
+      let formattedHours = Math.trunc(hours)
+      if (formattedSeconds < 10) {
+        formattedSeconds = `0${formattedSeconds}`;
+      };
+      if (formattedMinutes < 10) {
+        formattedMinutes = `0${formattedMinutes}`;
+      };
+      if (formattedHours < 10) {
+        formattedHours = `0${formattedHours}`;
+      };
+      if (seconds <= 0) {
+        clearInterval(createTimerAnimatorHandler);
+        timerEl.innerHTML = `00:00:00`;
+        alert('Время закончилось')
+      } else {
+        const strTimer = `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+        timerEl.innerHTML = strTimer;
+      };
+      --seconds;
+      },1000)
+    };
+  //  заводим таймер, через каждую секунду уменьшаем seconds на 1
+  // если значение однозначное добовляем ноль
+};
+
+const animateTimer = createTimerAnimator();
+
+inputEl.addEventListener('input', (event) => {
+  const parsedValue = parseInt(event.currentTarget.value, 10)
+  if (!Number.isInteger(parsedValue) || Number.isNaN(parsedValue)) {
+     event.currentTarget.value = '';
+  } else {
+     event.currentTarget.value = parsedValue;
+  }
+  // Очистите input так, чтобы в значении
+  // оставались только числа 
+});
+
+buttonEl.addEventListener('click', () => {
+  if (createTimerAnimatorHandler !== null) {
+    clearInterval(createTimerAnimatorHandler)
+  }
+  const seconds = Number(inputEl.value);
+  animateTimer(seconds);
+  
+
+  inputEl.value = '';
+});
